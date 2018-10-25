b1c2b607702c08b24f0cbaef7e374d019da10a62
bouj2468 <jeremie.bourque@usherbrooke.ca>
2018-10-25 11:00:08 -0400

    v2

--- src/main.ino
+++ src/main.ino
@@ -1,8 +1,41 @@
-#include "STEPPER.h"
+/*
+Projet: PIRUS S1
+Equipe: P-19
+Auteurs: -Jeremie Bourque
+Description: Robot de magasinage
+Date: 25-10-2018
+*/
+
+/* ****************************************************************************
+Inclure les librairies de functions que vous voulez utiliser
+**************************************************************************** */
+
 #include <Arduino.h>
+#include "stepper.h"
+#include "globals.h"
+
+/* ****************************************************************************
+Fonctions d'initialisation (setup)
+**************************************************************************** */
+
+void setup() 
+{
+  // Pour le systeme d'elevation.
+  Serial.begin(9600);
+  pinMode(pinDirection,OUTPUT); // Step
+  pinMode(pinStep,OUTPUT); // Dir
+  pinMode(boutonUp, INPUT_PULLUP); // Up
+  pinMode(boutonDown, INPUT_PULLUP); // Down
+  pinMode(boutonCalibration, INPUT_PULLUP); // Calibration
+}
+
+/* ****************************************************************************
+Fonctions de boucle infini (loop())
+**************************************************************************** */
 
 void loop() 
 {
+  // Tests systeme d'elevation.
   gotoEtage(2, 1);
   delay(1000);
   gotoEtage(0, 1);
@@ -10,6 +43,7 @@ void loop()
   gotoEtage(1, 1);
   delay(1000);
 
+  // Main.
   if(digitalRead(boutonUp) == LOW)
   {
     manuel(1);
